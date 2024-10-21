fc450e5694bb8b2d0188a1cc8bf9bda52dc504d9
Guilherme Guida <gguilhermecontato1@gmail.com>
2024-10-21 16:29:05 -0300

    Criação do findByDepartment.

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -15,6 +16,15 @@ public class Program {
         System.out.println("Seller by Id");
         Seller seller = sellerdao.findById(3);
         System.out.println(seller);
+
+        System.out.println(" ");
+
+        System.out.println("Seller By Department Id");
+        Department department = new Department(2, null);
+        List<Seller> list = sellerdao.finByDepartment(department);
+        for (Seller s : list) {
+            System.out.println(s);
+        }
 
     }
 }
